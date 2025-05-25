0b7913120748b1481b7315659b8510b3f7b715dd
Tobias Taurian Viana <eutobias@gmail.com>
2025-05-24 21:47:09 -0300

    chore: savepoint

---
+++ src/data/projects.data.ts
@@ -0,0 +1,55 @@
+type ProjectImages = {
+  src: string
+  alt: string
+}
+
+export type Project = {
+  name: string
+  description: string
+  images: ProjectImages[]
+}
+
+export const projects: Project[] = [
+  {
+    name: "Portfolio",
+    description:
+      "This portfolio website is built with Next.js and Tailwind CSS. It is hosted on Vercel and is fully responsive.",
+    images: [
+      {
+        src: "https://fakeimg.pl/600x400/000000/ffffff",
+        alt: "Portfolio 1",
+      },
+      {
+        src: "https://fakeimg.pl/600x400/000000/ffffff",
+        alt: "Portfolio 2",
+      },
+      {
+        src: "https://fakeimg.pl/600x400/000000/ffffff",
+        alt: "Portfolio 3",
+      },
+      {
+        src: "https://fakeimg.pl/600x400/000000/ffffff",
+        alt: "Portfolio 4",
+      },
+    ],
+  },
+  {
+    name: "E-commerce",
+    description:
+      "This e-commerce website is built with Next.js and Tailwind CSS. It is hosted on Vercel and is fully responsive.",
+    images: [
+      {
+        src: "https://fakeimg.pl/600x400/ffffff/000000",
+        alt: "E-commerce 1",
+      },
+      {
+        src: "https://fakeimg.pl/600x400/ffffff/000000",
+        alt: "E-commerce 2",
+      },
+      {
+        src: "https://fakeimg.pl/600x400/ffffff/000000",
+        alt: "E-commerce 3",
+      },
+    ],
+  }
+]
--- src/widgets/Projects/projects.component.tsx
+++ src/widgets/Projects/projects.component.tsx
@@ -1,17 +1,18 @@
 import { List } from "@/components/List";
 import { ProjectListItem } from "@/components/ProjectListitem";
+import { projects } from "@/data/projects.data";
 
 export const Projects = () => {
   return (
     <List className="mt-8">
       <List.Title>Projects</List.Title>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <List.Item key={i}>
+      {projects.map((project, index) => (
+        <List.Item key={`project-list-item-${index}`}>
           <ProjectListItem
-            title={`Project Title ${i}`}
-            content={`Project Description ${1}`}
-            imageUrl={i%2===0 ? "https://placehold.co/600x100": "https://placehold.co/600x400" }
-            imageAlt={`Thumb preview of project ${i}`}
+            title={project.name}
+            content={project.description}
+            imageUrl={project.images[0].src}
+            imageAlt={project.images[0].alt}
           />
         </List.Item>
       ))}
